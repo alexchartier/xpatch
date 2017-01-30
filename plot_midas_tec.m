10ce87f4f6f1c0961b6eb0ea660203a82028aea1
Alex Chartier <alex.chartier@outlook.com>
2017-01-30 15:02:34 -0500

    added matlab code

---
+++ plot_midas_tec.m
@@ -0,0 +1,6 @@
+%% plot_midas_tec.m
+% Script to plot the TEC output of MIDAS from a polar perspective
+
+%% Set input parameters
+IPath = '/Volumes/Seagate/data/swarm/midas/outPC10min3{ddmmmyy-HHMM}.mat';
+Times = datenum(2015, 12, 20, 17, 0, 0);
