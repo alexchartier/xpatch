8fd4a045c7d73d7d4b9023e41bc0646e51e77125
Alex Chartier <alex.chartier@outlook.com>
2017-02-02 18:00:46 -0500

    Got the plotting to work

--- plot_midas_tec.m
+++ plot_midas_tec.m
@@ -8,9 +8,9 @@ Sat = 'B';
 SwarmPath = [RootDir, 'lp/SW_OPER_EFI', Sat, ...
    '_PL_1B_{yyyymmdd}T000000_{yyyymmdd}T235959_0403_MDR_EFI_PL.cdf'];
 
-Time = datenum(2015, 12, 20, 17, 0, 0);
+Time = datenum(2015, 12, 20, 16, 40, 0);
+Window = 15/60/24;
 lat_cutoff = 55; 
-
 
 %% load
 D = tec(load(filename(IPath, Time)));
@@ -20,31 +20,64 @@ Lon = reshape(rad2deg(Sph(:, 3)), [length(D.Lat), length(D.Lon)]);
 
 Swarm_lat = cell2mat(cdfread(filename(SwarmPath, Time), 'variables', 'Latitude'));
 Swarm_lon = cell2mat(cdfread(filename(SwarmPath, Time), 'variables', 'Longitude'));
-
+Swarm_ne = cell2mat(cdfread(filename(SwarmPath, Time), 'variables', 'n'));
+Swarm_ne_err = cell2mat(cdfread(filename(SwarmPath, Time), 'variables', 'n_error'));
 Swarm_time = cdfread(filename(SwarmPath, Time), 'variables', 'Timestamp');
-Swarm_t = nan(length(Swarm_time));
+
+%% Swarm preprocessing
+Swarm_t = nan(length(Swarm_time), 1);
 for i = 1:length(Swarm_time)
    Swarm_t(i) = todatenum(Swarm_time{i});
 end
 
 latind = Swarm_lat > lat_cutoff;
-Swarm_t = Swarm_t(latind);
-Swarm_lon = Swarm_lon(latind);
-Swarm_lat = Swarm_lat(latind);
+tind = Swarm_t > Time - Window & Swarm_t < Time + Window;
 
+Swarm_t = Swarm_t(latind & tind);
+Swarm_lon = Swarm_lon(latind & tind);
+Swarm_lat = Swarm_lat(latind & tind);
+Swarm_ne = Swarm_ne(latind & tind);
+Swarm_ne_err = Swarm_ne_err(latind & tind);
 
 %% Plot
-m_proj('Stereographic', 'latitude', 90, 'radius', 35, 'rotation', -30)
-m_pcolor(Lon, Lat, squeeze(D.F));
-% set(hC,'LineStyle','none')
-title(datestr(D.Time))
-caxis([0 12]) % side colour axis
+subplot(2, 1, 1)
+colormap jet
+hold on
+m_proj('Stereographic', 'latitude', 90, 'radius', 35, 'rotation', 200)
+
+[~, hC] = m_contourf(Lon, Lat, squeeze(D.F), 100);
+set(hC,'LineStyle','none')
+m_coast('color', 'w', 'LineWidth', 1.2)
+title(filename('{dd/mm/yyyy HH:MM UT}                                            ', D.Time))
+caxis([0 14]) % side colour axis
 colorbar
 
-for i = 1:length(Swarm_lat)
-  m_plot(Swarm_lon(i), Swarm_lat(i), 'x')
-end
+m_grid('color', 'k', 'FontSize', 20)
+set(gca, 'FontSize', 20)
+blackline = m_plot(Swarm_lon, Swarm_lat, 'kx');
+uistack(blackline, 'top')
+hold off
 
-m_coast('color', 'w')
-m_grid('color', 'r', 'FontSize', 24)
-set(gca, 'FontSize', 24)
+subplot(2, 1, 2)
+plot(Swarm_t, Swarm_ne)
+xlim([min(Swarm_t), max(Swarm_t)])
+ylabel('Electron density (cm^-^3)')
+datetick('keeplimits')
+grid on
+grid minor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
