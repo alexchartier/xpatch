87424c46df0b4169a2f418401d101cee556a9150
Alex Chartier <alex.chartier@outlook.com>
2017-04-24 15:00:03 -0400

    Added EFI download

--- dl_swarm.py
+++ dl_swarm.py
@@ -16,15 +16,18 @@ cwd = os.getcwd()
 sys.path.append(cwd + '/../fusionpp/glimpse/')
 import downloader
 
-instrument = 'lp'
+instrument = 'efi'
 if instrument == 'gps':
     servername = 'data/swarm_server_names.txt'
     dl_dir = './data/swarm_tec/'
     # dl_dir = '/Volumes/Seagate/data/swarm/gps_tec/2015'
 elif instrument == 'lp':
     servername = 'data/swarm_lp.txt'
-    dl_dir = '~/xpatch/data/swarm_lp/'
-dl_times = [datetime.datetime(2015, 1, 1), datetime.datetime(2016, 1, 1)]
+    dl_dir = '/Volumes/usb/data/swarm_lp/'
+elif instrument == 'efi':
+    servername = 'data/swarm_efi.txt'
+    dl_dir = '/Volumes/usb/data/swarm_efi/'
+dl_times = [datetime.datetime(2016, 5, 7), datetime.datetime(2016, 5, 9)]
 dl_days = downloader.dl_data(dl_times, dl_dir, servername, datatype='swarm', dirnames='not_smart')
 
 
